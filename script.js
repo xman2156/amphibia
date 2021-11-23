cb56786ae01a2cba5a4863049acd85ad7b6ad971
xman2156 <34029777+xman2156@users.noreply.github.com>
2021-11-22 18:59:00 -0600

    Notice of manual changes needed and new episode data

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var oneDay = 24*60*60*1000;
-var latestRelease = new Date("2021-11-13T09:30:00-04:00"); //Newest Episode Release
-var nextRelease = new Date("2021-11-20T09:30:00-04:00"); // Next Episode release Comment out when no date
+var latestRelease = new Date("2021-11-20T09:30:00-06:00"); //Newest Episode Release
+var nextRelease = new Date("2021-11-27T09:30:00-06:00"); // Next Episode release Comment out when no date
 var mode = 0; //DD:HH:MM:SS mode is default
 var lastHiatusMention = null;
 
