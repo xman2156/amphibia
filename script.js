5ff8036b146cdd8bacaac2c49e263646ec567b28
xman2156 <34029777+xman2156@users.noreply.github.com>
2021-12-05 22:34:42 -0600

    Code cleanup

--- script.js
+++ script.js
@@ -180,7 +180,7 @@ window.setInterval(function(){
   timer("up", latestRelease, "count");
   timer("down", hiatusRankCheck(), "count2");
   timer("up", lastHiatusMention, "count3");
-  timer("down", nextRelease, "count4"); //Comment out when no new release date
+  timer("down", nextRelease, "count4");
 }, 250);
   
 //every 30 seconds, the most recent 100 posts on the subreddit are loaded up again in case there has been a new post that mentions hiatus
