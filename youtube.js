cca38c6a32f39ad3cbb5c64a5c7ae7231264a476
xman2156 <34029777+xman2156@users.noreply.github.com>
2021-12-12 11:06:36 -0600

    Updated YouTube video

--- youtube.js
+++ youtube.js
@@ -11,7 +11,7 @@ youtube.addEventListener( "click", function() {
 	var iframe = document.createElement( "iframe" );
 	iframe.setAttribute("frameborder", "0");
 	iframe.setAttribute("allowfullscreen", "");
-	iframe.setAttribute("src", "https://www.youtube.com/embed/YwWBa3L7lSo?autoplay=1");
+	iframe.setAttribute("src", "https://www.youtube.com/embed/ZqJ-uFDRk8c?autoplay=1");
 	this.innerHTML = "";
 	this.appendChild(iframe);
 });	
