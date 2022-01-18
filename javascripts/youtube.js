ef4c17d01ea0b7a7bf58581c2e50df1429b60229
xman2156 <xman2156@gmail.com>
2022-01-18 15:55:21 -0600

    Update YouTube video

--- javascripts/youtube.js
+++ javascripts/youtube.js
@@ -1,5 +1,5 @@
-var youtube = document.getElementById("youtube");		
-var source = "images/thumbnail.webp";	
+var youtube = document.getElementById("youtube");	//https://img.youtube.com/vi/enter video link/sddefault.jpg
+var source = "images/thumbnail.webp";
 var image = new Image();
 
 image.src = source;
@@ -11,7 +11,7 @@ youtube.addEventListener( "click", function() {
 	var iframe = document.createElement( "iframe" );
 	iframe.setAttribute("frameborder", "0");
 	iframe.setAttribute("allowfullscreen", "");
-	iframe.setAttribute("src", "https://www.youtube.com/embed/ZqJ-uFDRk8c?autoplay=1");
+	iframe.setAttribute("src", "https://www.youtube.com/embed/Ut7huRSR14M?autoplay=1");
 	this.innerHTML = "";
 	this.appendChild(iframe);
 });	
