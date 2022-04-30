1eb9a9175252c47690a0a4e2785f609bc3157c96
xman2156 <xman2156@gmail.com>
2022-04-29 21:38:02 -0500

    FInal episode is now the main countdown

--- javascripts/script.js
+++ javascripts/script.js
@@ -5,13 +5,14 @@ var episodeList = [
   ["Mother of Olms; Grime's Pupil", 'The Root of Evil; The Core & The King', new Date("2022-04-09T09:30:00-05:00"), new Date("2022-04-16T09:30:00-05:00")],
   ['The Root of Evil; The Core & The King', 'Newts in Tights; Fight or Flight', new Date("2022-04-16T09:30:00-05:00"), new Date("2022-04-23T09:30:00-05:00")],
   ['Newts in Tights; Fight or Flight', 'The Three Armies; The Beginning of the End', new Date("2022-04-23T09:30:00-05:00"), new Date("2022-04-30T09:30:00-05:00")],
-  ['The Three Armies; The Beginning of the End', '???', new Date("2022-04-30T09:30:00-05:00"), new Date("2022-05-07T09:30:00-05:00")],
+  ['The Three Armies; The Beginning of the End', 'The Hardest Thing', new Date("2022-04-30T09:30:00-05:00"), new Date("2022-05-07T09:30:00-05:00")],
 ];
 var startDate = new Date("2022-03-26T09:30:00-05:00");
 var today = Date.now();
 var weeksPassed = Math.floor((today - Date.parse(startDate))/(24*3600*1000*7));
 var latestRelease = episodeList[weeksPassed][2];
 var nextRelease = episodeList[weeksPassed][3];
+var finalRelease = new Date("2022-05-14T20:00:00-05:00");
 document.getElementById("previousEpisode").innerHTML = episodeList[weeksPassed][0];
 document.getElementById("nextEpisode").innerHTML = episodeList[weeksPassed][1]
 
@@ -190,6 +191,7 @@ function createTable(array) {
   
 //does the ticking
 window.setInterval(function(){
+  timer("down", finalRelease, "finalcount");
   timer("up", latestRelease, "count");
   //timer("down", hiatusRankCheck(), "count2");
   timer("up", lastHiatusMention, "count3");
